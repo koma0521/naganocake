d5e09bd6fc98c939213f9a043e146943b8b6bf12
vagrant <vagrant@localhost.localdomain>
2020-08-18 00:01:41 +0000

    [add]admin_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin, controllers: {
+    sessions:  'admin/sessions',
+    passwords:  'admin/passwords',
+    registrations:  'admin/registrations'
+  }
+  namespace :admin do
+    root 'top#top'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
